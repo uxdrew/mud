f062fbfa1c9cb9b2d7395b1b4f35ca7c8e639647
Jeff <jeffwise26@gmail.com>
2018-05-05 13:50:51 -0700

    working with plotly and subgraph

--- node/client/src/App.js
+++ node/client/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Plot from 'react-plotly.js';
 
 class App extends Component {
-  state = {graphs : [], userId : "alec"};
+  state = {graphs : [], userId : "alec",
+      data: [],
+      trace : { x:[1,2,3], y:[1,2,3]}, type: 'scatter',
+      mode: 'lines+points',
+      marker: {color: 'red'},
+      layout:{width: 320, height: 240, title: 'A Fancy Plot'}};
 
+  constructor(props) {
+      super(props);
+      this.state.data.push(this.state.trace);
+  }
   componentDidMount() {
     fetch('/graph?user=' + this.state.userId)
       .then(res => res.json())
@@ -19,6 +29,11 @@ class App extends Component {
 
             <div>
                 <iframe  width="900" height="800" frameBorder="0" scrolling="no" src={graph.url}></iframe>
+
+                <Plot
+                    data={this.state.data}
+                    layout={this.state.layout}
+                />
             </div>
         )}
       </div>
